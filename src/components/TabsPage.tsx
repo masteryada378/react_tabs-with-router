629968555c0ba93f071f1f62602a6358963642e0
masteryada378 <masteryada378@gmail.com>
2026-07-10 12:24:37 +0300

    upd final

--- src/components/TabsPage.tsx
+++ src/components/TabsPage.tsx
@@ -1,8 +1,10 @@
-import { useParams } from 'react-router-dom';
-import { Tabs } from './Tabs';
-import { Tab } from '../types/Tab';
+/* eslint-disable import/no-extraneous-dependencies */
+import { useParams, Link } from 'react-router-dom';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { Tab as TabType } from '../types/Tab';
 
-const tabsData: Tab[] = [
+const tabsData: TabType[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
@@ -11,17 +13,43 @@ const tabsData: Tab[] = [
 export const TabsPage = () => {
   const { tabId } = useParams<{ tabId: string }>();
 
-  const activeTab = tabsData.find(tab => tab.id === tabId);
+  const activeIndex = tabsData.findIndex(tab => tab.id === tabId);
+  const isTabValid = activeIndex !== -1;
 
   return (
     <>
       <h1 className="title">Tabs page</h1>
 
-      <Tabs tabs={tabsData} selectedTabId={tabId} />
+      <Tabs selectedIndex={activeIndex} onSelect={() => {}}>
+        <TabList>
+          {tabsData.map(tab => (
+            <Tab key={tab.id} data-cy="Tab" selectedClassName="is-active">
+              <Link
+                to={`/tabs/${tab.id}`}
+                style={{ display: 'block', outline: 'none' }}
+              >
+                {tab.title}
+              </Link>
+            </Tab>
+          ))}
+        </TabList>
 
-      <div className="block" data-cy="TabContent">
-        {activeTab ? activeTab.content : 'Please select a tab'}
-      </div>
+        {tabsData.map(tab => (
+          <TabPanel key={tab.id}>
+            {isTabValid && (
+              <div className="block" data-cy="TabContent">
+                {tab.content}
+              </div>
+            )}
+          </TabPanel>
+        ))}
+      </Tabs>
+
+      {!isTabValid && (
+        <div className="block" data-cy="TabContent">
+          Please select a tab
+        </div>
+      )}
     </>
   );
 };
